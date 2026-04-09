be148fa8a7ab6705279bb966dbf4fa7284c97417
firmanariyoga2-pixel <firman.ariyoga2006@gmail.com>
2026-04-09 17:07:43 +0800

    new web route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
