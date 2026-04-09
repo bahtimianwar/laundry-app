7b74f7334e9e88e1009cbabb7a787d92ab3e0b5e
Anwar <bahtimianwar140705@gmail.com>
2026-04-09 17:48:48 +0800

    route terbaru

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\LayananController;
 use App\Http\Controllers\TransaksiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
 // Menampilkan form login (Method GET)
 // middleware('guest') artinya halaman ini cuma bisa dibuka kalau user BELUM login
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
