551268d210eb0f66b7b80da8e3b4134ede7dbe8d
Anwar <bahtimianwar140705@gmail.com>
2026-04-09 17:19:39 +0800

    halaman login

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-// Menampilkan form login (Method GET)
-// middleware('guest') artinya halaman ini cuma bisa dibuka kalau user BELUM login
+// Redirect alamat utama langsung ke halaman login
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+// Route login yang kita buat tadi
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
-
-// Memproses form login saat disubmit (Method POST)
 Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
-
-// Memproses logout (Method POST)
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
