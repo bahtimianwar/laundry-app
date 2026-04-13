1350460157fc123a3734104d57e7c8a2f5b6470d
Anwar <bahtimianwar140705@gmail.com>
2026-04-13 10:39:26 +0800

    hapus seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            LaundrySeeder::class,
         ]);
 
         
