c29c745fe8daa4410be12bde532f4a5fd003a355
abidnahla11-design <abidnahla11@gmail.com>
2026-04-14 19:12:09 +0800

    update project

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\LayananController;
 use App\Http\Controllers\TransaksiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login');
 
 // Menampilkan form login (Method GET)
 // middleware('guest') artinya halaman ini cuma bisa dibuka kalau user BELUM login
